6362c1c88d3a0da962d8457671338ec95d124b66
davidkvistgaard <davidkvistgaard@gmail.com>
2026-02-19 10:47:34 +0100

    Create page.js

---
+++ app/team/leaderboards/page.js
@@ -0,0 +1,10 @@
+export default function LeaderboardsPage() {
+  return (
+    <main>
+      <h2 style={{ marginTop: 0 }}>Ranglister</h2>
+      <div style={{ border: "1px solid #eee", borderRadius: 14, padding: 14, opacity: 0.85 }}>
+        MVP: Her kommer points-ranglister, GC, ungdom, bjerg osv.
+      </div>
+    </main>
+  );
+}
