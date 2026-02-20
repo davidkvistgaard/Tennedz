7207abc4f252aa519bb0a68427cc64062c40c159
davidkvistgaard <davidkvistgaard@gmail.com>
2026-02-20 13:29:09 +0100

    Update layout.js

--- app/team/layout.js
+++ app/team/layout.js
@@ -1,5 +1,8 @@
-import TeamShell from "../components/TeamShell";
+// app/team/layout.js
 
-export default function Layout({ children }) {
-  return <TeamShell>{children}</TeamShell>;
+export default function TeamLayout({ children }) {
+  // IMPORTANT:
+  // Do NOT wrap TeamShell here, because the pages already use <TeamShell />
+  // Wrapping here would duplicate the header/nav.
+  return children;
 }
